751359bee9cd5fded5070af5838de97924202a7e
toltonic <grayson.a.jones@proton.me>
2026-04-17 20:02:00 -0700

    new

--- init.lua
+++ init.lua
@@ -3,17 +3,9 @@ local builtin = require('telescope.builtin')
 
 vim.opt.clipboard = "unnamedplus"
 
-local current_theme = "sonokai" -- set your default
 
-function ToggleColorscheme()
-	if current_theme == "sonokai" then
-		current_theme = "tokyonight"
-	else
-		current_theme = "sonokai"
-	end
+vim.cmd.colorscheme("tokyonight-moon")
 
-	vim.cmd.colorscheme(current_theme)
-end
 
 
 
@@ -21,7 +13,10 @@ vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader><leader>', ':Ex<CR>')
 vim.keymap.set('n', '<leader>f', builtin.find_files, {desc = 'Telescope find files'})
 vim.keymap.set('n', '<leader>o', 'o<ESC>')
-vim.keymap.set("n", "<leader>c", ToggleColorscheme, { desc = "Toggle colorscheme" })
+
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
 
 
 vim.opt.number = true
